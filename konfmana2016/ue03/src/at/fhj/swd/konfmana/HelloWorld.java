825e67d1153746007363d0542e7721f552ed0192
Christian Schindler <cschindler@ist.tugraz.at>
2016-05-13 23:02:57 +0200

    just some minor comments added

--- konfmana2016/ue03/src/at/fhj/swd/konfmana/HelloWorld.java
+++ konfmana2016/ue03/src/at/fhj/swd/konfmana/HelloWorld.java
@@ -1,21 +1,41 @@
 package at.fhj.swd.konfmana;
 
+/**
+ * a simple hello world class
+ * @author cschindler
+ *
+ */
 public class HelloWorld {
 
 	private String name=null;
 
+	/**
+	 * constructor initializing the name
+	 * @param name to be initialized
+	 */
 	public HelloWorld(String name){
 		setName(name);
 	}
 	
+	/**
+	 * default constructor
+	 */
 	public HelloWorld(){
 		setName(null);
 	}
 	
+	/**
+	 * returns the name which appears in the hello world greetings when set
+	 * @return the name 
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * sets the name which appear in the hello world greetings
+	 * @param name to be set
+	 */
 	public void setName(String name) {
 		if (name!=null){
 			this.name = name.trim();
